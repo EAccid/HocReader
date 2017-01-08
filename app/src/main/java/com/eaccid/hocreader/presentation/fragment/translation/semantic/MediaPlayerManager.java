ea17511c3a7e8c03a0f59e86e3d4dd51e07fc2c7
EAccid <elena.briginets@gmail.com>
2017-01-08 21:18:04 +0200

    add media player manager

---
+++ app/src/main/java/com/eaccid/hocreader/presentation/fragment/translation/semantic/MediaPlayerManager.java
@@ -0,0 +1,28 @@
+package com.eaccid.hocreader.presentation.fragment.translation.semantic;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+public class MediaPlayerManager {
+
+    public MediaPlayer createAndPreparePlayerFromURL(final String url) {
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mediaPlayer;
+    }
+
+    public void play(final MediaPlayer mediaPlayer) {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+        mediaPlayer.start();
+    }
+}
