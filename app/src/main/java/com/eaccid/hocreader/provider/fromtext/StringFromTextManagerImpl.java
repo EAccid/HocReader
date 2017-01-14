13a18c8b34d1172ccc04fdf0123b7db931dc99a8
EAccid <elena.briginets@gmail.com>
2017-01-14 20:59:00 +0200

    add check on null

--- app/src/main/java/com/eaccid/hocreader/provider/fromtext/StringFromTextManagerImpl.java
+++ app/src/main/java/com/eaccid/hocreader/provider/fromtext/StringFromTextManagerImpl.java
@@ -3,6 +3,8 @@ package com.eaccid.hocreader.provider.fromtext;
 public class StringFromTextManagerImpl implements StringFromTextManager {
     @Override
     public String capitalizeFirsChar(String text) {
+        if (text == null || text.isEmpty())
+            return "";
         StringBuilder sb = new StringBuilder(text);
         sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
         return sb.toString();
