60a30bc8e4ac74f812aea9f91fa1688fed03990e
EAccid <elena.briginets@gmail.com>
2017-01-19 20:50:51 +0200

    add temp content view to memorizing card activity and created presenter: todo - refactor presenter to fetch and handle data from interactor

--- app/src/main/java/com/eaccid/hocreader/presentation/activity/notcard/CardWordActivity.java
+++ app/src/main/java/com/eaccid/hocreader/presentation/activity/notcard/CardWordActivity.java
@@ -1,28 +1,112 @@
 package com.eaccid.hocreader.presentation.activity.notcard;
 
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.eaccid.hocreader.R;
+import com.eaccid.hocreader.presentation.BasePresenter;
+import com.eaccid.hocreader.presentation.BaseView;
+import com.eaccid.hocreader.presentation.fragment.translation.semantic.ImageViewManager;
+import com.eaccid.hocreader.provider.db.WordItem;
+import com.eaccid.hocreader.provider.db.WordItemProvider;
+import com.eaccid.hocreader.underdevelopment.IconTogglesResourcesProvider;
+import com.eaccid.hocreader.underdevelopment.MemorizingCalculatorImpl;
+import com.eaccid.hocreader.underdevelopment.ReaderExceptionHandlerImpl;
 
-public class CardWordActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CardWordActivity extends AppCompatActivity implements BaseView {
+
+    @BindView(R.id.word)
+    TextView word;
+    @BindView(R.id.word_transcription)
+    TextView transcription;
+    @BindView(R.id.translation)
+    TextView translation;
+    @BindView(R.id.word_image)
+    ImageView wordImage;
+    @BindView(R.id.action_delete)
+    ImageView deleteWord;
+    @BindView(R.id.already_learned)
+    ImageView alreadyLearned;
+    @BindView(R.id.transcription_speaker)
+    ImageView transcriptionSpeaker;
+    MediaPlayer mediaPlayer;
+    @BindView(R.id.don_t_know)
+    Button dontKnow;
+    @BindView(R.id.remember)
+    Button remember;
+    private static CardWordPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_memorizing);
+        setContentView(R.layout.word_item_fragment_2);
+        if (mPresenter == null) mPresenter = new CardWordPresenter();
+        mPresenter.attachView(this);
         setFinishOnTouchOutside(true);
-        String word = getIntent().getStringExtra("word");
-        String translation = getIntent().getStringExtra("translation");
+        ButterKnife.bind(this);
 
-        TextView tv_word = (TextView) findViewById(R.id.word);
-        TextView tv_translation = (TextView) findViewById(R.id.translation);
+        /**TODO data from presenter, temp solution*/
+        WordItem wordItem = mPresenter.getWordItem();
+        setDataToViewFromItem(wordItem);
+        setListenersToViewFromItem(wordItem);
+    }
 
-        tv_word.setText(word);
-        tv_translation.setText(translation);
+    private void setListenersToViewFromItem(WordItem wordItem) {
+        new WordItemProvider().getWordItemWithTranslation(wordItem)
+                .subscribe(item -> {
+                    word.setText(item.getWordFromText());
+                    new ImageViewManager().loadPictureFromUrl(wordImage, item.getPictureUrl());
+                    transcription.setText("[" + item.getTranscription() + "]");
+                    alreadyLearned.setImageResource(
+                            new IconTogglesResourcesProvider().getAlreadyLearnedWordResId(
+                                    new MemorizingCalculatorImpl(item)
+                            )
+                    );
+                }, e -> {
+                    new ReaderExceptionHandlerImpl().handleError(e);
+                });
+    }
 
+    private void setDataToViewFromItem(WordItem wordItem) {
+        deleteWord.setOnClickListener(v -> {
+        });
+        dontKnow.setOnClickListener(v -> {
+            translation.setText(getTranslation());
+            translation.setTextColor(Color.parseColor("#fff45f30"));
+        });
+        remember.setOnClickListener(v -> {
+            translation.setText(getTranslation());
+            translation.setTextColor(Color.parseColor("#ff2f8b2a"));
+        });
+        transcriptionSpeaker.setOnClickListener(v -> {
+        });
+    }
+
+    public String getWord() {
+        return getIntent().getStringExtra("word");
+    }
+
+    public String getTranslation() {
+        return getIntent().getStringExtra("translation");
+    }
+
+    @Override
+    public BasePresenter getPresenter() {
+        return mPresenter;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detachView();
     }
 
 }
--- app/src/main/java/com/eaccid/hocreader/presentation/activity/notcard/CardWordPresenter.java
+++ app/src/main/java/com/eaccid/hocreader/presentation/activity/notcard/CardWordPresenter.java
@@ -1,4 +1,47 @@
 package com.eaccid.hocreader.presentation.activity.notcard;
 
-public class CardWordPresenter {
+import android.util.Log;
+
+import com.eaccid.hocreader.data.local.AppDatabaseManager;
+import com.eaccid.hocreader.data.local.db.entity.Word;
+import com.eaccid.hocreader.injection.App;
+import com.eaccid.hocreader.presentation.BasePresenter;
+import com.eaccid.hocreader.provider.db.WordItem;
+import com.eaccid.hocreader.provider.db.WordItemImpl;
+
+import javax.inject.Inject;
+
+public class CardWordPresenter implements BasePresenter<CardWordActivity> {
+
+    private final String logTAG = "MainPresenter";
+    private CardWordActivity mView;
+
+    //TODO inject provider instead of manager
+    @Inject
+    AppDatabaseManager databaseManager;
+
+    public CardWordPresenter() {
+        App.getAppComponent().inject(this);
+    }
+
+    @Override
+    public void attachView(CardWordActivity cardWordActivity) {
+        mView = cardWordActivity;
+        Log.i(logTAG, "CardWordActivity has been attached.");
+    }
+
+    @Override
+    public void detachView() {
+        Log.i(logTAG, "CardWordActivity has been detached.");
+        mView = null;
+    }
+
+    public WordItem getWordItem() {
+        //todo temp solution, del after refactor setListenersToViewFromItem in View
+        Word word = databaseManager.getWord(mView.getWord());
+        if (word == null)
+            word = new Word();
+        WordItemImpl wordItem = new WordItemImpl(word);
+        return wordItem;
+    }
 }
