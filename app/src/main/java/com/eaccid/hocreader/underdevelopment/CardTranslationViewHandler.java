4aebf161074fddb932740412da8e0ad13ad99f25
EAccid <elena.briginets@gmail.com>
2017-01-23 17:35:09 +0200

    add new class with todo description

---
+++ app/src/main/java/com/eaccid/hocreader/underdevelopment/CardTranslationViewHandler.java
@@ -0,0 +1,12 @@
+package com.eaccid.hocreader.underdevelopment;
+
+public class CardTranslationViewHandler {
+
+    /**TODO get word_provider data and set to view
+
+     WordCarouselRecyclerViewAdapter
+     SwipeOnLongPressRecyclerViewAdapter
+     CardWordActivity
+     */
+
+}
