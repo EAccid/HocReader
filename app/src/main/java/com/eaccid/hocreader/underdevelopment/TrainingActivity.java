ef0eb3f7ab9fe5c17a3edd98cca96941cdca24f7
EAccid <elena.briginets@gmail.com>
2017-01-31 16:03:37 +0200

    change packages

--- app/src/main/java/com/eaccid/hocreader/underdevelopment/TrainingActivity.java
+++ app/src/main/java/com/eaccid/hocreader/underdevelopment/TrainingActivity.java
@@ -1,11 +1,13 @@
-package com.eaccid.hocreader.presentation.training;
+package com.eaccid.hocreader.underdevelopment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.eaccid.hocreader.R;
 import com.eaccid.hocreader.presentation.FragmentTags;
+import com.eaccid.hocreader.presentation.training.WordTrainingFragment;
 
+//TODO temp activity, refactor to separate TrainingFragment
 public class TrainingActivity extends AppCompatActivity {
 
     @Override
