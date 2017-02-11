4560d30b2cd4859c220e981a1169358a785bd160
EAccid <elena.briginets@gmail.com>
2017-02-12 00:36:18 +0200

    refactor SearchSuggestionsProvider

--- app/src/main/java/com/eaccid/hocreader/presentation/main/serchadapter/SearchSuggestionsProvider.java
+++ app/src/main/java/com/eaccid/hocreader/presentation/main/serchadapter/SearchSuggestionsProvider.java
@@ -2,12 +2,12 @@ package com.eaccid.hocreader.presentation.main.serchadapter;
 
 import android.content.SearchRecentSuggestionsProvider;
 
-public class SearchSuggestionsProvider extends SearchRecentSuggestionsProvider  {
+public class SearchSuggestionsProvider extends SearchRecentSuggestionsProvider {
 
-    public final static String AUTHORITY = "SearchSuggestionsProvider";
-    public final static int MODE = DATABASE_MODE_QUERIES;
+    public final static String AUTHORITY = "com.eaccid.hocreader.presentation.main.serchadapter.SearchSuggestionsProvider";
+    public final static int MODE = DATABASE_MODE_QUERIES | DATABASE_MODE_2LINES;
 
     public SearchSuggestionsProvider() {
-            setupSuggestions(AUTHORITY, MODE);
+        setupSuggestions(AUTHORITY, MODE);
     }
 }
